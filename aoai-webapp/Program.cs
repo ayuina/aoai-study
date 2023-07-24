a20ea54bf7279765ed7fad26c28152965b577657
Ayumu Inaba <ayumu.inaba@microsoft.com>
2023-07-24 16:53:07 +0900

    init aoai code

--- aoai-webapp/Program.cs
+++ aoai-webapp/Program.cs
@@ -1,7 +1,17 @@
+using Azure.AI.OpenAI;
+using Azure;
+using Azure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton<OpenAIClient>(
+        new OpenAIClient(
+            new Uri(builder.Configuration["AOAI:Endpoint"] ?? ""), 
+            new DefaultAzureCredential())
+);
 
 var app = builder.Build();
 
@@ -10,10 +20,10 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    //app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
